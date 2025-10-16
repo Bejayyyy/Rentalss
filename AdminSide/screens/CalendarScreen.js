355a4404271c26b31b98656a54c7f075da4b9271
Bejayyyy <josephigot219@gmail.com>
2025-10-17 01:10:15 +0800

    initail commit'

--- AdminSide/screens/CalendarScreen.js
+++ AdminSide/screens/CalendarScreen.js
@@ -903,12 +903,12 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     position: 'relative',
-    marginTop: 2,
+    marginTop: 0,
   },
   vehicleImage: {
     width: '100%',
     height: '100%',
-    borderRadius: 4,
+    borderRadius: 0,
   },
   countBadge: {
     position: 'absolute',
